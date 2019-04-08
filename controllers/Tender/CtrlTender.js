9a65ce983055491790612f5e7ede69592f3ce14e
JeanCazaux <jeancazaux@hotmail.com>
2019-04-08 19:23:31 +0200

    feat: 20190408 tenders list

--- controllers/Tender/CtrlTender.js
+++ controllers/Tender/CtrlTender.js
@@ -10,8 +10,20 @@ exports.TenderGet = (req, res) => {
     }).catch((err) => { require(process.cwd() + '/controllers/CtrlTool').onError(err, res) })
 }
 
+exports.TenderList = (req, res) => {
+    require(process.cwd() + '/controllers/Tender/MdlTender').TenderList(req.body.id, req.body.algoliaId, req.body.creationDateMin, req.body.creationDateMax, req.body.termDateMin, req.body.termDateMax).then((data) => {
+        res.end(JSON.stringify({ success: true, data: data }, null, 3))
+    }).catch((err) => { require(process.cwd() + '/controllers/CtrlTool').onError(err, res) })
+}
+
 exports.TenderRemove = (req, res) => {
     require(process.cwd() + '/controllers/Tender/MdlTender').TenderRemove(req.body.id, req.body.algoliaId).then((data) => {
+        res.end(JSON.stringify({ success: true, data: data }, null, 3))
+    }).catch((err) => { require(process.cwd() + '/controllers/CtrlTool').onError(err, res) })
+}
+
+exports.TenderStatistic = (req, res) => {
+    require(process.cwd() + '/controllers/Tender/MdlTender').TenderStatistic(req.body.year, req.body.month).then((data) => {
         res.end(JSON.stringify({ success: true, data: data }, null, 3))
     }).catch((err) => { require(process.cwd() + '/controllers/CtrlTool').onError(err, res) })
 }
--- controllers/Tender/MdlTender.js
+++ controllers/Tender/MdlTender.js
@@ -170,6 +170,126 @@ exports.TenderGet = (id, algoliaId) => {
   })
 }
 
+exports.TenderList = (id, algoliaId, creationDateMin, creationDateMax, termDateMin, termDateMax) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const config = require(process.cwd() + '/config')
+      const BddTool = require(process.cwd() + '/global/BddTool')
+
+      const BddId = 'deepbloo'
+      const BddEnvironnement = config.prefixe
+
+      let query = `
+        SELECT      id AS "id",
+                    dgmarketId AS "dgmarketId",
+                    procurementId AS "procurementId",
+                    title AS "title",
+                    description AS "description",
+                    lang AS "lang",
+                    contactFirstName AS "contactFirstName",
+                    contactLastName AS "contactLastName",
+                    contactAddress AS "contactAddress",
+                    contactCity AS "contactCity",
+                    contactState AS "contactState",
+                    contactCountry AS "contactCountry",
+                    contactEmail AS "contactEmail",
+                    contactPhone AS "contactPhone",
+                    buyerName AS "buyerName",
+                    buyerCountry AS "buyerCountry",
+                    procurementMethod AS "procurementMethod",
+                    noticeType AS "noticeType",
+                    country AS "country",
+                    estimatedCost AS "estimatedCost",
+                    currency AS "currency",
+                    publicationDate AS "publicationDate",
+                    cpvs AS "cpvs",
+                    cpvDescriptions AS "cpvDescriptions",
+                    words AS "words",
+                    bidDeadlineDate AS "bidDeadlineDate",
+                    sourceUrl AS "sourceUrl",
+                    termDate AS "termDate",
+                    fileSource AS "fileSource",
+                    userId AS "userId",
+                    algoliaId AS "algoliaId",
+                    status AS "status",
+                    creationDate AS "creationDate",
+                    updateDate AS "updateDate"
+        FROM        dgmarket 
+      `
+      let where = ``
+      if (id && id !== '' && id > 0) {
+        if (where !== '') { where += 'AND ' }
+        where += `id = ${BddTool.NumericFormater(id, BddEnvironnement, BddId)} \n`
+      }
+      if (algoliaId && algoliaId !== '' && algoliaId > 0) {
+        if (where !== '') { where += 'AND ' }
+        where += `algoliaId = ${BddTool.NumericFormater(algoliaId, BddEnvironnement, BddId)} \n`
+      }
+      if (creationDateMin && creationDateMin !== '') {
+        if (where !== '') { where += 'AND '}
+        where += `creationDate >= ${BddTool.DateFormater(creationDateMin, BddEnvironnement, BddId)} `
+      }
+      if (creationDateMax && creationDateMax !== '') {
+        if (where !== '') { where += 'AND '}
+        where += `creationDate <= ${BddTool.DateFormater(creationDateMax, BddEnvironnement, BddId)} `
+      }
+      if (termDateMin && termDateMin !== '') {
+        if (where !== '') { where += 'AND '}
+        where += `termDate >= ${BddTool.DateFormater(termDateMin, BddEnvironnement, BddId)} `
+      }
+      if (termDateMax && termDateMax !== '') {
+        if (where !== '') { where += 'AND '}
+        where += `termDate <= ${BddTool.DateFormater(termDateMax, BddEnvironnement, BddId)} `
+      }
+      if (where !== '') { query += 'WHERE ' + where }
+      let recordset = await BddTool.QueryExecBdd2(BddId, BddEnvironnement, query)
+      let tenders = []
+      for (var record of recordset) {
+        tenders.push({
+          id: record.id,
+          dgmarketId: record.dgmarketId,
+          procurementId: record.procurementId,
+          title: record.title,
+          description: record.description,
+          lang: record.lang,
+          contactFirstName: record.contactFirstName,
+          contactLastName: record.contactLastName,
+          contactAddress: record.contactAddress,
+          contactCity: record.contactCity,
+          contactState: record.contactState,
+          contactCountry: record.contactCountry,
+          contactEmail: record.contactEmail,
+          contactPhone: record.contactPhone,
+          buyerName: record.buyerName,
+          buyerCountry: record.buyerCountry,
+          procurementMethod: record.procurementMethod,
+          noticeType: record.noticeType,
+          country: record.country,
+          estimatedCost: record.estimatedCost,
+          currency: record.currency,
+          publicationDate: record.publicationDate,
+          cpvs: record.cpvs,
+          cpvDescriptions: record.cpvDescriptions,
+          words: record.words,
+          bidDeadlineDate: record.bidDeadlineDate,
+          sourceUrl: record.sourceUrl,
+          termDate: record.termDate,
+          fileSource: record.fileSource,
+          userId: record.userId,
+          algoliaId: record.algoliaId,
+          status: record.status,
+          creationDate: record.creationDate,
+          updateDate: record.updateDate
+        })
+      }
+
+      resolve(tenders)
+    } catch (err) {
+      reject(err)
+    }
+  })
+}
+
 exports.TenderRemove = (id, algoliaId) => {
   return new Promise(async (resolve, reject) => {
     try {
@@ -205,6 +325,49 @@ exports.TenderRemove = (id, algoliaId) => {
       await require(process.cwd() + '/controllers/Algolia/MdlAlgolia').TendersImport()
 
       resolve(tender)
+    } catch (err) {
+      reject(err)
+    }
+  })
+}
+
+exports.TenderStatistic = (year, month) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let creationDateMin = new Date(year, month, 1);
+      let creationDateMax = new Date(year, month, 1);
+      creationDateMax.setMonth(creationDateMax.getMonth() + 1);
+      creationDateMax.setDate(creationDateMin.getDate() - 1);
+      let termDateMin = new Date(year, month, 1);
+      let tenders = await this.TenderList(null, null, null, null, termDateMin)
+      let statistic = {
+        count: tenders.length,
+        countrys: [],
+      }
+      for (let tender of tenders) {
+        if (tender.country && tender.country !== '') {
+          let country = statistic.countrys.find(a => a.country === tender.country)
+          if(!country) {
+            statistic.countrys.push({
+              country: tender.country,
+              count: 1,
+            });
+          } else {
+            country.count++
+          }
+        }
+      }
+      statistic.countrys.sort((a, b) => {
+        if (a.count > b.count) {
+          return 1;
+        }
+        if (a.count < b.count) {
+          return -1;
+        }
+        return 0;
+      });
+      statistic.countrys.reverse();
+      resolve(statistic)
     } catch (err) {
       reject(err)
     }
