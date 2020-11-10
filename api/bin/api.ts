737ba05141abb663f7ecae7ad8d2364489f00d93
Olivier Barbut <olivier@artisaninformaticien.fr>
2020-11-10 16:05:00 +0100

    Cleanup

--- api/bin/api.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { ApiStack } from '../lib/api-stack';
-
-const app = new cdk.App();
-new ApiStack(app, 'ApiStack', { env: { account: "669031476932", region: "eu-west-1" }});
