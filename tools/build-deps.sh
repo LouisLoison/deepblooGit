66bb95e5a7bda3aab57df9cdb7bd07c1e51623a4
Olivier Barbut <olivier@artisaninformaticien.fr>
2020-11-19 16:16:47 +0100

    No more bin symlink, bin fixes

--- tools/build-deps.sh
+++ tools/build-deps.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -e
 
-(cd lambda/libjs && npm pack)
+# (cd lambda/libjs && npm pack)
 
 sudo docker run --rm -v "$PWD":/var/task lambci/lambda:build-python3.8 bash tools/build-deps-python.sh
 
 sudo docker run --rm -v "$PWD":/var/task lambci/lambda:build-nodejs12.x bash tools/build-deps-node.sh
+
 
 mkdir -p lambda/layer/npm/nodejs/node_modules/deepblooback/
 cp -a ../deepbloo-back/{controllers,config,global} lambda/layer/npm/nodejs/node_modules/deepblooback/
@@ -13,9 +14,12 @@ cp -a ../deepbloo-back/{controllers,config,global} lambda/layer/npm/nodejs/node_
 mkdir -p lambda/layer/npm/nodejs/node_modules/deepbloo/
 cp -a lambda/libjs/* lambda/layer/npm/nodejs/node_modules/deepbloo/
 
+
+sudo rm $(find lambda/layer/pipenv/bin/ -type l)
+
 rm -fr lambda/layer/combined/{python,nodejs,bin}
-mkdir -p lambda/layer/combined/
-cp -au lambda/layer/*/python lambda/layer/combined/
-cp -au lambda/layer/*/nodejs lambda/layer/combined/
-cp -au lambda/layer/*/bin lambda/layer/combined/
+mkdir -p lambda/layer/combined/{python,nodejs,bin}
+cp -au lambda/layer/*/python/* lambda/layer/combined/python
+cp -au lambda/layer/*/nodejs/* lambda/layer/combined/nodejs
+cp -au lambda/layer/*/bin/* lambda/layer/combined/bin
 
