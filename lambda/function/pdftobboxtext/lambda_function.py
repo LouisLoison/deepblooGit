de4b5d42a714ee50695c6e823a02622a35ce44a1
Mille <valentin.mille@epitech.eu>
2020-12-17 16:52:49 +0100

    [Add] pdfToBbox by lines

--- lambda/function/pdftobboxtext/lambda_function.py
+++ lambda/function/pdftobboxtext/lambda_function.py
@@ -1,52 +1,83 @@
 import json
 import os
+from helper import AwsHelper, S3Helper
+from PdfToBbox import Pdf
 import pdfplumber
-import subprocess
-from helper import AwsHelper
+import simplejson
 
 
-def get_bbox_filename(path_to_pdf: str, document_id: str) -> str:
-    folder_output = "/".join(path_to_pdf.split('/')[:-1])  # get path without file
-    path_without_ext, _ = os.path.splitext(path_to_pdf.split('/')[-1])
-    pdf_output = path_without_ext + '.pdf'
-    output_file = "{}-analysis/{}/{}".format(folder_output, document_id, pdf_output)
-    return output_file
+def send_message(client, qUrl, json_message) -> None:
+    message = json.dumps(json_message)
+    client.send_message(QueueUrl=qUrl, MessageBody=message)
+    print("Message to queue: {}".format(qUrl))
+    print("Submitted message to queue: {}".format(message))
 
 
-def execute_pdf_to_bbox(pdf_tmp_path: str, bbox_output: str, output_format="json", output_type="line") -> int:
+def send_to_textract(aws_env: dict):
+    json_message = {
+        "bucketName": aws_env["outputBucket"],
+        "objectName": aws_env["outputName"],
+        "documentId": aws_env["documentId"],
+        "awsRegion": aws_env["awsRegion"]
+    }
+    client = AwsHelper().getClient('sqs', awsRegion=aws_env["awsRegion"])
+    qUrl = aws_env['textractQueueUrl']
+    send_message(client, qUrl, json_message)
+
+
+def get_bbox_filename(path_to_pdf: str) -> str:
+    folder_output, pdf_output = os.path.split(path_to_pdf)
+    file_name, ext = os.path.splitext(pdf_output)
+    json_file = file_name + ".json"
+    json_output = os.path.join(folder_output, json_file)
+    return json_output
+
+
+def write_bbox_to_s3(aws_env: dict) -> None:
+    with open(aws_env['tmpJsonOutput'], "r") as file:
+        content = file.read()
+        S3Helper.writeToS3(content, aws_env['outputBucket'], aws_env['outputName'], aws_env['awsRegion'])
+
+
+def execute_pdf_to_bbox(pdf_tmp_path: str, bbox_output: str, output_format="json", output_type="line") -> bool:
     """
     Execute the pdfplumber binary with pdf_tmp_path to extract bounding boxes.
 
     :param pdf_tmp_path: the tmp path of the pdf to pass to pdfplumber
-    :param output_format: the output format of the bounding boxes. Can be "json" or "csv"
-    :param output_type: object types to extract. "char", "rect", "line", "curve", "image", "annot"
-    :return: None
+    :param bbox_output: the tmp file of the json to create and to stock pdf information
+    :return: int
     """
-    available_format = ["json", "csv"]
-    available_types = ["char", "rect", "line", "curve", "image", "annot"]
-    if output_format not in available_format:
-        print("[pdfplumber execution] => Wrong format parameter given {0}".format(output_format))
-        return 0
-    if output_type not in available_types:
-        print("[pdfplumber execution] => Wrong type parameter given {0}".format(output_type))
-    pdfplumber_cmd = ["pdfplumber", "--format", output_format, output_type]
-    commands = [*pdfplumber_cmd, "< {}".format(pdf_tmp_path), "> {0}".format(bbox_output)]
-    try:
-        subprocess.call(commands, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    except subprocess.CalledProcessError as error:
-        print("[EXIT CODE] : {0}\n[DESCRIPTION]  => {1}".format(error.returncode, error.stdout))
-        return error.returncode
-    return 0
+    with pdfplumber.open(pdf_tmp_path) as pdf_file:
+        page_pdf = []
+        if os.path.isfile(bbox_output):
+            os.remove(bbox_output)
+        with open(bbox_output, "w") as json_file:
+            for page in pdf_file.pages:
+                page_pdf.append({
+                    "page_number": page.page_number,
+                    "page_content": page.extract_words(x_tolerance=3, y_tolerance=3, keep_blank_chars=False,
+                                                       use_text_flow=False, horizontal_ltr=True, vertical_ttb=True,
+                                                       extra_attrs=[])
+                })
+            json_pretty_content = simplejson.dumps(page_pdf, indent=4)
+            json_file.write(json_pretty_content)
+    return False
 
 
-def write_bbox_to_s3(aws_env: dict) -> None:
-    with open(aws_env['outputName']) as file:
-        bbox_content = file.read()
-        AwsHelper.writeTos3(bbox_content, aws_env['bucketName'], aws_env['objectName'], aws_env['awsRegion'])
+def is_valid_pdf(pdf_path, min_char_required) -> bool:
+    with pdfplumber.open(pdf_path) as pdf_content:
+        images = pdf_content.images
+        if len(images) == 0:
+            return False
+        for page in pdf_content.pages:
+            nb_char_in_page = len(page.chars)
+            if nb_char_in_page < min_char_required:
+                return False
+    return True
 
 
 def copy_pdf_to_tmp(aws_env: dict) -> str:
-    pdf_content = AwsHelper.readFroms3(aws_env['bucketName'], aws_env['objectName'], aws_env['awsRegion'])
+    pdf_content = S3Helper.readFromS3(aws_env['bucketName'], aws_env['objectName'], aws_env['awsRegion'])
     tmp_folder = "/tmp"
     pdf_tmp = "tmp_0.pdf"
     index = 0
@@ -56,8 +87,9 @@ def copy_pdf_to_tmp(aws_env: dict) -> str:
         pdf_tmp = "tmp_{0}.pdf".format(index)
         index += 1
     pdf_tmp = os.path.join(tmp_folder, pdf_tmp)
-    with open(pdf_tmp) as tmp_file:
+    with open(pdf_tmp, "w") as tmp_file:
         tmp_file.write(pdf_content)
+    print("Copy {0} to {1}".format(aws_env["objectName"], pdf_tmp))
     return pdf_tmp
 
 
@@ -69,13 +101,33 @@ def lambda_handler(event, context):
         "objectName": body['objectName'],
         "documentId": body['documentId'],
         "awsRegion": aws_region,
+        "tmpJsonOutput": "/tmp/tmp_result.json",
         "outputBucket": os.environ['OUTPUT_BUCKET'],
-        "outputName": get_bbox_filename(body['objectName'], body['documentId'])
+        "outputName": get_bbox_filename(body['objectName']),
+        "textractQueueUrl": os.environ['ELASTIC_QUEUE_URL'],
+        "textractOnly": os.environ['TEXTRACT_ONLY'],
+        "minCharNeeded": os.environ['MIN_CHAR_NEEDED'],
+        "extract_pdf_lines": os.environ['EXTRACT_PDF_LINES']
     }
-    pdf_tmp_path = copy_pdf_to_tmp(aws_env)
-    execute_pdf_to_bbox(pdf_tmp_path, aws_env["outputName"])
-    write_bbox_to_s3(aws_env)
-    return {
+    status = {
         'statusCode': 200,
         'body': 'All right'
     }
+    pdf_tmp_path = copy_pdf_to_tmp(aws_env)
+    if aws_env['textractOnly'] == "false" and is_valid_pdf(pdf_tmp_path, aws_env['minCharNeeded']) is False:
+        print("=> Extracting bounding box without textract")
+        if aws_env['extract_pdf_lines'] is True:
+            print("=> Extracting pdf lines bbox")
+            pdf = Pdf(pdf_tmp_path, aws_env['tmpJsonOutput'])
+            pdf.parse_pdf()
+            pdf.save_in_json()
+        else:
+            print("=> Extracting pdf words bbox")
+            if execute_pdf_to_bbox(pdf_tmp_path, aws_env['tmpJsonOutput']):
+                print("=> Error while trying to get pdf information")
+                return status
+        write_bbox_to_s3(aws_env)
+    else:
+        print("=> Extracting bounding box with textract")
+        send_to_textract(aws_env)
+    return status
---
+++ lambda/layer/helper/python/PdfToBbox.py
@@ -0,0 +1,145 @@
+import pdfplumber
+import os
+import json
+from decimal import Decimal
+
+
+
+class Pdf:
+    def __init__(self, pdf_path, json_output_path, y_tolerance=3):
+        self.parser: PdfBboxLineParser = PdfBboxLineParser(pdf_path, y_tolerance)
+        self.json_folder_path, self.json_name = os.path.split(json_output_path)
+        self.pages: [PdfPage] = []
+
+    def parse_pdf(self):
+        self.pages = self.parser.parse_pdf_file()
+
+    def dump_pdf(self):
+        for page in self.pages:
+            page.dump_pdf_page()
+
+    def save_in_json(self):
+        pdf_pages = []
+        json_path = os.path.join(self.json_folder_path, self.json_name)
+        for page in self.pages:
+            pdf_pages.append(page.get_page_in_json())
+        pdf_json = json.dumps(pdf_pages, indent=4, ensure_ascii=False)
+        with open(json_path, "w", encoding='utf-8') as json_file:
+            json_file.write(pdf_json)
+
+
+class PdfPage:
+    def __init__(self, page_nb):
+        self.pdf_lines: [PdfLine] = []
+        self.page_nb = page_nb
+
+    def add_line(self, pdf_plumber_line: [dict]):
+        pdf_line = PdfLine()
+        pdf_line.convert_line(pdf_plumber_line)
+        self.pdf_lines.append(pdf_line)
+
+    def get_page_in_json(self):
+        json_pdf_line = []
+        for line in self.pdf_lines:
+            json_pdf_line.append(line.get_line_in_json())
+        return {
+            "page_number": self.page_nb,
+            "page_content": json_pdf_line
+        }
+
+    def dump_pdf_page(self):
+        print("=========== Page {} ===========".format(self.page_nb))
+        for line in self.pdf_lines:
+            line.dump_pdf_line()
+
+
+class PdfBboxLineParser:
+    def __init__(self, pdf_path, y_tolerance):
+        # folder_path = "path/to/folder" / pdf_file_name = "pdf_name.pdf"
+        self.folder_path, self.pdf_file_name = os.path.split(pdf_path)
+        self.y_tolerance = y_tolerance
+
+    def parse_pdf_file(self) -> [PdfPage]:
+        pdf_path = os.path.join(self.folder_path, self.pdf_file_name)
+        index = 1
+        pdf_pages: [PdfPage] = []
+
+        with pdfplumber.open(pdf_path) as pdf_file:
+            for page in pdf_file.pages:
+                page_words = page.extract_words(x_tolerance=3, y_tolerance=3, keep_blank_chars=False,
+                                                use_text_flow=False, horizontal_ltr=True, vertical_ttb=True,
+                                                extra_attrs=[])
+                pdf_pages.append(self.extract_page_lines(page_words, index))
+                index += 1
+        return pdf_pages
+
+    def is_same_line(self, line_top: Decimal, line_bot: Decimal, current_top: Decimal, current_bot: Decimal) -> bool:
+        top_diff = line_top - current_top
+        bot_diff = line_bot - current_bot
+        if not -self.y_tolerance < top_diff < self.y_tolerance:
+            return False
+        if not -self.y_tolerance < bot_diff < self.y_tolerance:
+            return False
+        return True
+
+    def extract_page_lines(self, extracted_words, nb_page) -> [PdfPage]:
+        index = 0
+        pdf_words_line = []
+        pdf_page: PdfPage = PdfPage(nb_page)
+
+        if len(extracted_words) == 0 or len(extracted_words) == 1:
+            return [extracted_words]
+        line_top = extracted_words[index].get("top")
+        line_bot = extracted_words[index].get("bottom")
+        while index < len(extracted_words):
+            if (self.is_same_line(line_top, line_bot, extracted_words[index].get("top"), extracted_words[index].get(
+                    "bottom")) is True):
+                pdf_words_line.append(extracted_words[index])
+            else:
+                line_top = extracted_words[index].get("top")
+                line_bot = extracted_words[index].get("bottom")
+                pdf_page.add_line(pdf_words_line)
+                pdf_words_line = [extracted_words[index]]
+            index += 1
+        # Create the last line of the page
+        pdf_page.add_line(pdf_words_line)
+        return pdf_page
+
+
+class PdfLine:
+    def __init__(self):
+        self.line = ""
+        self.size = len(self.line)
+        self.x0 = 0
+        self.x1 = 0
+        self.y0 = 0
+        self.y1 = 0
+
+    def convert_line(self, words: [dict]):
+        nb_word = len(words)
+        if nb_word != 0:
+            self.x0 = float(words[0].get('x0'))
+            self.y0 = float(words[0].get('top'))
+            for word in words:
+                self.line += "{} ".format(word.get("text"))
+            self.size = len(self.line)
+            self.x1 = float(words[nb_word - 1].get('x1'))
+            self.y1 = float(words[nb_word - 1].get('bottom'))
+
+    def get_line_in_json(self):
+        return {
+            "text": self.line,
+            "size": self.size,
+            "x0": self.x0,
+            "x1": self.x1,
+            "top": self.y0,
+            "bottom": self.y1,
+        }
+
+    def dump_pdf_line(self):
+        print("[Line] {}".format(self.line))
+        print("[Size] {}".format(self.size))
+        print("[x0] {}".format(self.x0))
+        print("[x1] {}".format(self.x1))
+        print("[y0] {}".format(self.y0))
+        print("[y1] {}\n".format(self.y1))
