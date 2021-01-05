eaf6ea101c280dfddc3c57a3650665a98f60189e
ericdasse28 <christ.gououi@gmail.com>
2021-01-05 05:09:58 +0100

    Add: class Quantity

---
+++ lambda/function/valueextraction/classes.py
@@ -0,0 +1,53 @@
+"""
+This module implements a class Quantity designed specifically
+for the needs of the project
+"""
+from xml.sax.expatreader import AttributesImpl
+from builtins import float
+
+
+class Quantity:
+    """Class for a quantity (e.g. 2.5 kW)
+    
+    Attributes
+    ----------
+    value : float
+        value of the quantity
+        
+    unit : str
+        unit of the quantity
+        
+    entity : str
+        Entity represented by the unit (e.g. energy potential,
+        currency...)
+        
+    uri : str
+        WikiPedia URI
+    """
+    
+    def __init__(self, value, unit, entity, uri=None):
+        """
+        Parameters
+        ----------
+        value : float
+            value of the quantity
+        
+        unit : str
+            unit of the quantity
+        
+        entity : str
+            Entity represented by the unit (e.g. energy potential,
+            currency...)
+        
+        uri : str, optional
+            WikiPedia URI
+        """
+        self.value = value
+        self.unit = unit
+        self.entity = entity
+        self.uri = uri
+        
+    def 
+        
+    
+     
