79fe0392ea0c4b1fc25e9f3cbb4b3caa46becb29
ericdasse28 <christ.gououi@gmail.com>
2021-01-05 04:32:00 +0100

    Add: extraction module
    This module contains functions to extract numerical values from text

---
+++ lambda/function/valueextraction/extraction.py
@@ -0,0 +1,26 @@
+"""Numerical Values Extraction
+
+This module implements functions to extract numerical values from text
+"""
+
+from quantulum3 import parser
+
+
+def extract_values(txt):
+    """
+    Extract numerical values from the input text
+    
+    Attributes
+    ----------
+    txt : str
+        Text from which the numerical values are to be extracted
+        
+    Returns
+    -------
+    quants
+        a list of objects each representing a numerical value
+    """
+     
+    quants = parser.parse(txt)
+    
+    return quants
