77ddf4c2960d1d5bd494b9ece005573cc159206e
Mille <valentin.mille@epitech.eu>
2021-01-08 10:40:16 +0100

    [Add] htmlToPdf functional with step function

--- lambda/function/htmltopdf/lambda_function.py
+++ lambda/function/htmltopdf/lambda_function.py
@@ -6,24 +6,6 @@ from xhtml2pdf import pisa
 from helper import FileHelper, S3Helper, AwsHelper
 
 
-def send_message(client, qUrl, json_message) -> None:
-    message = json.dumps(json_message)
-    client.send_message(QueueUrl=qUrl, MessageBody=message)
-    print("Submitted message to queue: {}".format(message))
-
-
-def send_to_pdf_to_bbox_lambda(aws_env: dict) -> None:
-    json_message = {
-        "bucketName": aws_env["outputBucket"],
-        "objectName": aws_env["outputName"],
-        "documentUuid": aws_env["documentUuid"],
-        "awsRegion": aws_env["awsRegion"]
-    }
-    client = AwsHelper().getClient('sqs', awsRegion=aws_env["awsRegion"])
-    qUrl = aws_env['pdfToBboxQueueUrl']
-    send_message(client, qUrl, json_message)
-
-
 def convert_html_to_pdf(html_str, aws_env) -> None:
     aws_region = aws_env['awsRegion']
     output_bucket = aws_env['outputBucket']
@@ -64,17 +46,15 @@ def get_pdf_filename(path_to_pdf: str, document_id: str) -> str:
 
 
 def lambda_handler(event, context):
-    body = json.loads(event['Records'][0]['body'])
-    aws_region = 'eu-west-1'
     print("==> Event: {0}".format(json.dumps(event)))
     aws_env = {
-        "bucketName": body['DOCUMENTS_BUCKET'],
-        "objectName": body['objectName'],
-        "documentUuid": body['documentUuid'],
-        "awsRegion": aws_region,
-        "outputBucket": os.environ['OUTPUT_BUCKET'],
-        #"pdfToBboxQueueUrl": os.environ['PDFTOBOUNDINGBOXANDTEXT_QUEUE_URL'],
-        "outputName": get_pdf_filename(body['objectName'], body['documentUuid'])
+        "bucketName": os.environ['DOCUMENTS_BUCKET'],
+        "objectName": event['objectName'],
+        "documentUuid": event['documentUuid'],
+        "awsRegion": 'eu-west-1',
+        "outputBucket": os.environ['DOCUMENTS_BUCKET'],
+        "outputName": get_pdf_filename(event['objectName'],
+                                       event['documentUuid'])
     }
     print("==> Aws env: {0}".format(aws_env))
     status = {
@@ -83,5 +63,4 @@ def lambda_handler(event, context):
     }
     html_content = read_from_s3(aws_env)
     convert_html_to_pdf(html_content, aws_env)
-    send_to_pdf_to_bbox_lambda(aws_env)
     return { **event, 'status': status, 'objectName': aws_env['outputName'] }
