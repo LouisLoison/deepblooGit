3d409135eeb51bdfc7cf345ac2c84ae60c904483
ericdasse28 <christ.gououi@gmail.com>
2021-01-11 12:51:19 +0100

    Feat: docstrings to explain the functions

--- lambda/function/valueextraction/data_preprocessing.py
+++ lambda/function/valueextraction/data_preprocessing.py
@@ -3,12 +3,15 @@ import re
 
 
 def clean_html(raw_html):
+    """Remove HTML tags from a text"""
+    
     clean_regex = re.compile('<.*?>')
     clean_text = re.sub(clean_regex, '', raw_html)
     return clean_text
 
 
 def process_column(column_name, dataframe, process):
-    # Define a method to apply the same process to every
-    # row in a pandas DataFrame column
+    """Define a method to apply the same process to every row in a
+    pandas DataFrame column"""
+
     dataframe[column_name] = pd.DataFrame(map(process, dataframe[column_name]))
