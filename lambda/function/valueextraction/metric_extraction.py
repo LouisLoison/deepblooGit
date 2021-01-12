6f7047b29579c002fe5b332b50566297ee17590e
ericdasse28 <christ.gououi@gmail.com>
2021-01-12 12:00:08 +0100

    Feat: dimensional filtering
    By modifying the extract_metrics function, I intend to make possible to
    only select metrics representing the dimensions we are interested in

--- lambda/function/valueextraction/metric_extraction.py
+++ lambda/function/valueextraction/metric_extraction.py
@@ -8,22 +8,39 @@ from quantulum3 import parser
 from classes import Metric
 
 
-def extract_metrics(txt):
+def extract_metrics(txt, dimensions=["power", "electrical potential",
+                                     "current", "length"]):
     """
     Extract metrics from the input text
+    Only metrics representing the input dimensions are selected
+    
     
     Attributes
     ----------
     txt : str
         Text from which the numerical values are to be extracted
+    dimensions: list, optional
+        List of dimensions (entities). Only metrics representing those
+        dimensions are selected within text
+        
+        By default, the supported dimensions are "power" (eg. 25.4 W), 
+        "electrical potential" (eg. 400 kV), "current" (53 A) and 
+        "length" (eg. 100m)
+        
+        All the supported dimensions are referenced in unit_references.csv
         
     Returns
     -------
     quants
         a list of objects each representing a numerical value
     """
-     
+    
+    # Metric extraction as quantulum3 Quantity objects
     quants = parser.parse(txt)
+    # Dimension filtering
+    quants = [quant for quant in quants 
+              if quants.unit.entity.name in dimensions]
+    # Mapping of the Quantity objects to Metric objects
     quants = map(quantulum_to_metric, quants)
     
     return quants
