f86a696da64346d58aa21028356e785a39d09bed
Olivier Barbut <olivier@artisaninformaticien.fr>
2021-02-04 15:21:59 +0100

    Loads spacy model at correct time

--- tools/build-deps-python.sh
+++ tools/build-deps-python.sh
@@ -1,17 +1,18 @@
 mkdir -p lambda/layer/pipenv/python
 cd lambda/layer/pipenv
 
-# To download the model needed by spacy if not installed
-pipenv run python -m spacy download xx_ent_wiki_sm
 
 (pipenv --version || pip3 install pipenv --user || pip install pipenv --user) && ( [ -e Pipfile.lock ] || pipenv lock ) && pipenv sync --bare
+
+# To download the model needed by spacy if not installed
+pipenv run python -m spacy download xx_ent_wiki_sm
 
 PIPENV=$(pipenv --venv)/lib/python3.8/site-packages
 
 mkdir bin 2>/dev/null
 
 pwd
-cp -a $(pipenv --venv)/bin/* bin
+# cp -a $(pipenv --venv)/bin/* bin
 
 rm $(find bin/ -type l) 
 
