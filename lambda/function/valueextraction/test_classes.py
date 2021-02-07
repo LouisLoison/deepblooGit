c652f7b51e07350d0d23a55531ddfda2a4194b9a
ericdasse28 <christ.gououi@gmail.com>
2021-02-07 22:55:21 +0100

    [Feat] Instantiate a unit with erroneous entity for later test
    This version includes added comments

--- lambda/function/valueextraction/test_classes.py
+++ lambda/function/valueextraction/test_classes.py
@@ -6,18 +6,21 @@ class TestClassUnit(unittest.TestCase):
     def setUp(self):
         self.unit1 = Unit("kilowatt", "power")
         self.unit2 = Unit("kW", "power")
+        self.unit3 = Unit("ampere", "time")
 
     def test_attributes(self):
         """Test whether the attributes were correctly assigned"""
+        # Test with unit full name
         self.assertEqual(self.unit1.name, "kilowatt")
         self.assertEqual(self.unit1.entity, "power")
         self.assertEqual(self.unit1.ref_unit, "watt")
         self.assertEqual(self.unit1.uri, "")
-
+        # Test with abbreviated unit
         self.assertEqual(self.unit2.name, "kW")
         self.assertEqual(self.unit2.entity, "power")
         self.assertEqual(self.unit2.ref_unit, "watt")
         self.assertEqual(self.unit2.uri, "")
+        # TODO: Test for inadequate entity
 
 
 class TestClassMetric(unittest.TestCase):
