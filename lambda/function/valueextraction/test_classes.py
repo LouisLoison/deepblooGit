9e6d973392e3dda4227ab06fc650663daec7be34
ericdasse28 <christ.gououi@gmail.com>
2021-02-07 18:09:11 +0100

    [Add] Test case for string cast in test_classes.py

--- lambda/function/valueextraction/test_classes.py
+++ lambda/function/valueextraction/test_classes.py
@@ -1,12 +1,13 @@
 import unittest
+from classes import *
 
 
 class TestClassMetric(unittest.TestCase):
     def setUp(self):
         self.metric = Metric(25.4, "kilowatt", "power", "25kW")
 
-    def test_init(self):
-        self.assertEqual(True, False)
+    def test_str(self):
+        self.assertEqual(str(metric), "25.40 kilowatt")
 
 
 if __name__ == '__main__':
