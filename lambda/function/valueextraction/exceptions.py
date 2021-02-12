5488acbaabb9ef8a8010ddce3b710fe1e3533827
ericdasse28 <christ.gououi@gmail.com>
2021-02-12 12:45:48 +0100

    [Add] UnitNotFoundException in exceptions.py

--- lambda/function/valueextraction/exceptions.py
+++ lambda/function/valueextraction/exceptions.py
@@ -4,3 +4,8 @@
 class EntityException(Exception):
     def __init__(self):
         Exception.__init__(self, "Defined entity does not match the associated unit")
+
+
+class UnitNotFoundException(Exception):
+    def __init__(self, unit_name):
+        Exception.__init__(self, "Unit {} does not exist in the registry.".format(unit_name))
