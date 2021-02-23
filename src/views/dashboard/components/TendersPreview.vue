1bbd93907abaaf17d6c5eccef84a1d19c96fe5e3
JeanCazaux <jeancazaux@hotmail.com>
2021-02-23 19:09:58 +0100

    feat: Dashboard add Refinement V3

--- src/views/dashboard/components/TendersPreview.vue
+++ src/views/dashboard/components/TendersPreview.vue
@@ -1,6 +1,75 @@
 <template>
   <div>
-    <TendersRefinement />
+    <v-app-bar
+      color="grey lighten-4"
+      dense
+    >
+      <span class="text-h5 font-weight-bold">{{ dashboard.name }}</span>
+
+      <v-spacer />
+
+      <v-btn
+        @click="showTendersRefinement = !showTendersRefinement"
+        small
+        icon
+        :color="showTendersRefinement ? 'blue-grey' : 'grey'"
+        title="Show tenders filter"
+      >
+        <v-icon small>fa-filter</v-icon>
+      </v-btn>
+      <v-btn
+        @click="updateDisplay('EDIT')"
+        small
+        icon
+        color="blue-grey"
+        title="Edit"
+      >
+        <v-icon small>fa-edit</v-icon>
+      </v-btn>
+      <v-btn
+        @click="search()"
+        small
+        icon
+        color="blue-grey"
+        title="Refresh"
+      >
+        <v-icon small>fa-sync-alt</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <TendersRefinement
+      v-if="searchFilter && showTendersRefinement"
+      :searchInputValue="searchFilter.searchInputValue"
+      :filter="searchFilter.facets"
+      @searchInputValueRemove="searchInputValueRemove()"
+      @facetItemRemove="facetItemRemove($event)"
+    />
+    <div v-if="!dataSearch.loading" class="text-center pt-5">
+      <v-progress-circular :size="50" color="blue-grey lighten-4" indeterminate />
+    </div>
+    <v-list
+      v-else
+      dense
+      three-line
+    >
+      <template v-for="(result, index) in this.dataSearch.data.results">
+        <v-list-item
+          :key="`result${index}`"
+          @click="openTender(result)"
+        >
+          <v-list-item-content>
+            <v-list-item-title>
+              <v-avatar class="mr-1" size="16">
+                <v-img :src="$global.cpvLogo(result.cpvs.raw)" />
+              </v-avatar>
+              {{ $global.htmlText(result.title.raw) }}
+            </v-list-item-title>
+            <v-list-item-subtitle>
+              {{ $global.htmlText(result.description.raw) }}
+            </v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </template>
+    </v-list>
   </div>
 </template>
 
@@ -8,7 +77,7 @@
 import TendersRefinement from '@/views/tender/components/TendersRefinement'
 
 export default {
-  name: 'ItemCard',
+  name: 'TendersPreview',
 
   components: {
     TendersRefinement,
@@ -20,41 +89,41 @@ export default {
       required: false,
     },
 
-    selectedUuid: {
-      type: String,
-      required: false,
-    },
-
-    filter: {
+    dashboard: {
       type: Object,
       required: false,
     },
 
     dataSearch: {
       type: Object,
-      required: true,
+      required: false,
     },
 
-    item: {
+    searchFilter: {
       type: Object,
-      required: true,
+      required: false,
     },
   },
 
   data: () => ({
+    showTendersRefinement: false,
   }),
 
   methods: {
-    selectItem(item) {
-      this.$emit('selectItem', item)
-    },
-
-    deleteItem(item) {
-      this.$emit('deleteItem', item)
+    updateDisplay(value) {
+      this.$emit('updateDisplay', value)
     },
 
     openTender(tender) {
       this.$emit('openTender', tender)
+    },
+
+    searchInputValueRemove() {
+      this.$emit('searchInputValueRemove')
+    },
+
+    facetItemRemove(event) {
+      this.$emit('facetItemRemove', event)
     },
   },
 }
