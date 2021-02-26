4ea58c1d7f3a25f871ab09e97cdc7d1421b5691d
Mille <valentin.mille@epitech.eu>
2021-02-26 17:04:47 +0100

    feat: add aurora postgres stack, remove .png

--- cdk/bin/deepbloo-app.ts
+++ cdk/bin/deepbloo-app.ts
@@ -5,12 +5,17 @@ import { TextractPipelineStack } from '../lib/textract-pipeline-stack';
 import { ApiStack } from '../lib/api-stack';
 import { TenderStack } from '../lib/tender-stack';
 import { DocumentStack } from '../lib/document-stack';
+import { AuroraDbStack } from '../lib/auroradb-stack';
 
 const app = new cdk.App();
 
 const apiStack = new ApiStack(app, 'ApiStack', { env: { account: "669031476932", region: "eu-west-1" }});
 
 const textractPipelineStack = new TextractPipelineStack(app, 'TextractPipelineStack', { env: { account: "669031476932", region: "eu-west-1" }});
+
+const auroraDbStack = new AuroraDbStack(app, 'AuroraPostgresStack', {
+  env: { account: "669031476932", region: "eu-west-1"}
+})
 
 const documentStack = new DocumentStack(app, 'DocumentStack', {
   env: { account: "669031476932", region:   "eu-west-1" },
--- cdk/lib/tender-stack-props.ts
+++
@@ -1,6 +0,0 @@
-import { StackProps } from '@aws-cdk/core';
-
-export interface TenderStackProps extends StackProps {
-  readonly documentStackArn: string;
-  readonly documentStackName: string;
-}
