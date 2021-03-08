bd44ccb00fe0016e475153ee6b177e52eb8b6122
Olivier Barbut <olivier@artisaninformaticien.fr>
2021-03-08 14:52:01 +0100

    test

--- lambda/test/libjs/metricranges.unit.js
+++ lambda/test/libjs/metricranges.unit.js
@@ -1,14 +1,16 @@
+const { metricsRanges, extractMetrics, metricsCriterions } = require('../../libjs/metricranges')
 
 test('metricRangesTest', done => {
 
-  const { metricsRanges, extractMetrics, metricsCriterions } = require('../metricranges')
 
   let ranges = metricsRanges('electric potential', [{ entity: 'electric potential', numericValue: 22333 }, { entity: 'power', numericValue: 22333 }])
-  console.log(ranges)
+  // console.log(ranges)
+  expect(ranges).toEqual(["MV (1kV/33kV)"])
 
   ranges = metricsRanges('power', [{ entity: 'electric potential', numericValue: 22333 }, { entity: 'power', numericValue: 22333 }])
+  expect(ranges).toEqual(["1kW/1MW"])
 
-  console.log(ranges)
+  // console.log(ranges)
 
 
   extractMetrics({
@@ -16,15 +18,15 @@ test('metricRangesTest', done => {
     description: 'The power handled is 40MW. A 20/11KV transformer and a 420/210 kV one, linked by a 30 - 40 KVa line over 34 kms of distance, about 23 miles. Repeated A 20/11KV transformer and a 420/210 kV one, linked by a 30 40 KVa line over 34 kms of distance, about 23 m',
   }).then(metrics => {
 
-    console.log(metrics)
+    // console.log(metrics)
     const criterions = metricsCriterions(metrics)
-    console.log(criterions)
+    // console.log(criterions)
     ranges = metricsRanges('electric potential', criterions)
-    console.log(ranges)
+    // console.log(ranges)
     expect(ranges).toEqual(['EHV >(330kV)', 'EHV >(220kV)', 'HV (33kV/220kV)', 'MV (1kV/33kV)'])
     ranges = metricsRanges('power', criterions)
-    console.log(ranges)
+    // console.log(ranges)
     expect(ranges).toEqual(['10MW/100MW', '1kW/1MW'])
     done()
   })
-})
+}, 15000)
