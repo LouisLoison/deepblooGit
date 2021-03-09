824445bf59242b905aa286050a514df606f3d309
Mille <valentin.mille@epitech.eu>
2021-03-09 17:22:06 +0100

    feat: checks before downloading document in db

--- lambda/function/stepDocumentDownload/index.js
+++ lambda/function/stepDocumentDownload/index.js
@@ -1,6 +1,9 @@
 const { tenderFileImport } = require('deepbloo').document
 exports.handler =  async function(event, ) {
   console.log(event)
-  const { tenderUuid, sourceUrl, tenderId } = event
-  return await tenderFileImport(tenderUuid, sourceUrl, tenderId)
+  const { tenderUuid, sourceUrl, tenderId, filename, status } = event
+  if (tenderUuid && filename && status) {
+    return await tenderFileImport(tenderUuid, sourceUrl, tenderId)
+  }
+  return event
 }
