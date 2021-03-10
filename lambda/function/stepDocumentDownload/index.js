bc12fd1bae72ccd27a3fe3b515cbc4259f5dea76
Mille <valentin.mille@epitech.eu>
2021-03-10 09:13:40 +0100

    fix: merging from develop

--- lambda/function/stepDocumentDownload/index.js
+++ lambda/function/stepDocumentDownload/index.js
@@ -1,9 +1,6 @@
 const { tenderFileImport } = require('deepbloo').document
 exports.handler =  async function(event, ) {
   console.log(event)
-  const { tenderUuid, sourceUrl, tenderId, filename, status } = event
-  if (tenderUuid && filename && status) {
-    return await tenderFileImport(tenderUuid, sourceUrl, tenderId)
-  }
-  return event
+  const { tenderUuid, sourceUrl, tenderId } = event
+  return await tenderFileImport(tenderUuid, sourceUrl, tenderId)
 }
