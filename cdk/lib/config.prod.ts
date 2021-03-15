1df017d105c009e4acfc04ef750d0b9bce82406c
Olivier Barbut <olivier@artisaninformaticien.fr>
2021-03-15 14:19:50 +0100

    Prod configs

--- cdk/lib/config.prod.ts
+++ cdk/lib/config.prod.ts
@@ -1,16 +1,19 @@
 
 const config = {
-  NODE_ENV: 'dev',
-  DB_SECRET: 'arn:aws:secretsmanager:eu-west-1:669031476932:secret:aurora-creds-faJRvx',
-  DB_HOST: 'serverless-test.cluster-cxvdonhye3yz.eu-west-1.rds.amazonaws.com',
+  account: '957571717716',
+  region: 'eu-west-1',
+  dbName: 'db',
+  NODE_ENV: 'prod',
+  DB_SECRET: 'arn:aws:secretsmanager:eu-west-1:957571717716:secret:aurora-TRgv8E',
+  DB_HOST: 'db.cluster-c6pgcparadyn.eu-west-1.rds.amazonaws.com',
   APPSEARCH_ENDPOINT: 'https://7bbe91f62e1e4ff6b41e5ee2fba2cdbd.app-search.eu-west-1.aws.found.io/',
   APPSEARCH_SECRET: 'arn:aws:secretsmanager:eu-west-1:669031476932:secret:appsearch-TZnQcu',
   ELASTIC_SECRET: "arn:aws:secretsmanager:eu-west-1:669031476932:secret:elastic-fnVFZr",
-  vpcId: 'vpc-f7456f91',
+  vpcId: 'vpc-a262b9db',
   availabilityZones: ['eu-west-1a', 'eu-west-1b', 'eu-west-1c'],
-  privateSubnetIds: ['subnet-0d44e4d2296bfd59f', 'subnet-0530f274ce7351e90', 'subnet-0530f274ce7351e90'],
+  privateSubnetIds: ['subnet-3f332759', 'subnet-a89572e3', 'subnet-02257458'],
   DOCUMENTS_BUCKET: 'textractpipelinestack-documentsbucket9ec9deb9-mla8aarhzynj',
-  HIVEBRITE_SECRET: 'arn:aws:secretsmanager:eu-west-1:669031476932:secret:hivebrite-tayvUB',
+  HIVEBRITE_SECRET: 'arn:aws:secretsmanager:eu-west-1:957571717716:secret:hivebrite-In9VfQ',
 }
 
 export {
