f6998c76dcc1d7737f65cd5f5adde51b88fc23c5
Olivier Barbut <olivier@artisaninformaticien.fr>
2021-03-17 09:59:17 +0100

    Easyer env script

--- tools/env.sh
+++ tools/env.sh
@@ -4,6 +4,10 @@ TOPLEVEL=`git rev-parse --show-toplevel`
 [ -z $NODE_ENV ] && NODE_ENV=dev
 export TOPLEVEL NODE_ENV
 
-`cat $TOPLEVEL/cdk/lib/config.ts  |grep '^  '|sed "s/ *: *'/=/"|sed 's/^ *//'| sed "s/',$//" |grep '^[A-Z]' |sed 's/^/export /'`
-
-# APPSEARCH_PRIVATE=`aws secretsmanager get-secret-value --secret-id $APPSEARCH_SECRET |jq -r '.SecretString | fromjson | .appsearchPrivateKey'`
+export DB_SECRET=arn:aws:secretsmanager:eu-west-1:669031476932:secret:aurora-creds-faJRvx
+export DB_HOST=serverless-test.cluster-cxvdonhye3yz.eu-west-1.rds.amazonaws.com
+export APPSEARCH_ENDPOINT=https://7bbe91f62e1e4ff6b41e5ee2fba2cdbd.app-search.eu-west-1.aws.found.io/
+export APPSEARCH_SECRET=arn:aws:secretsmanager:eu-west-1:669031476932:secret:appsearch-TZnQcu
+export ELASTIC_SECRET=arn:aws:secretsmanager:eu-west-1:669031476932:secret:elastic-fnVFZr
+export DOCUMENTS_BUCKET=textractpipelinestack-documentsbucket9ec9deb9-mla8aarhzynj
+export HIVEBRITE_SECRET=arn:aws:secretsmanager:eu-west-1:669031476932:secret:hivebrite-tayvUB
