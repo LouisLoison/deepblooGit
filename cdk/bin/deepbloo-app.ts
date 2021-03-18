741b1545854a2bac1db375f0eaf42ef76ff9edc2
Olivier Barbut <olivier@artisaninformaticien.fr>
2021-03-18 13:46:14 +0100

    Simplify stacks

--- cdk/bin/deepbloo-app.ts
+++ cdk/bin/deepbloo-app.ts
@@ -1,15 +1,12 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import { App }  from '@aws-cdk/core';
-import { TextractPipelineStack } from '../lib/textract-pipeline-stack';
 import { ApiStack } from '../lib/api-stack';
 import { TenderStack } from '../lib/tender-stack';
 import { DocumentStack } from '../lib/document-stack';
 import { DocumentCfStack } from '../lib/document-cf-stack';
-import { AuroraDbStack } from '../lib/auroradb-stack';
-import { FrontendStack } from '../lib/frontend-stack';
-// import { CloudFrontStack } from '../lib/cloud-front-stack';
-
+// import { AuroraDbStack } from '../lib/auroradb-stack';
+// import { FrontendStack } from '../lib/frontend-stack';
 
 import { config } from '../lib/config';
 
@@ -23,8 +20,6 @@ new ApiStack(app, 'ApiStack', { env });
 
 // new FrontendStack(app, 'FrontendStack', { env });
 new DocumentCfStack(app, 'DocumentCfStack', { env });
-
-new TextractPipelineStack(app, 'TextractPipelineStack', { env });
 
 // const cloudFrontStack = new CloudFrontStack(app, 'DocumentAccessStack', { env: { account: "669031476932", region: "eu-west-1" }});
 
